6ebd5ab083935e95ac218d06f29b2f9a15b2d0ff
janek <user@smseagle.eu>
2024-01-12 12:38:57 +0100

    utworzenie plikow do zadania 5

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("funkcja.php");
+
+?>
+<html>
+<head>
+   <title>Przeszukiwacz stron</title>
+</head>
+<body>
+<?php
+
+   zdobadz_email("https://kursphp.com");
+
+?>
+<p>Wiadomość została wysłana</p>
+</body>
+</html>
