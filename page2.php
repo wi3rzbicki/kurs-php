4c2ac72e0c0f9d8819a9f45a97182029cffb7b5d
janek <user@smseagle.eu>
2024-01-19 14:28:59 +0100

    dodanie bootstrapa

--- page2.php
+++ page2.php
@@ -7,6 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
    <?php
@@ -23,7 +24,7 @@ session_start();
         Wiek: <input type="text" name="wiek" value="<?php echo @$_SESSION['wiek']; ?>"/><br/>
         <input type="submit" value="OK" /><br/>
    </form>
-   <button onclick="location.href='page1.php'">poprzednia</button>
-   <button onclick="location.href=href='page3.php'">nastepna</button>
+   <button class="btn btn-primary" onclick="location.href='page1.php'">poprzednia</button>
+   <button class="btn btn-success" onclick="location.href=href='page3.php'">nastepna</button>
 </body>
 </html>
--- page3.php
+++ page3.php
@@ -7,10 +7,11 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-   <button onclick="location.href=href='page2.php'">poprzednia</button>
-   <button onclick="location.href=href='page1.php'">nastepna</button>
+   <button class="btn btn-primary" onclick="location.href=href='page2.php'">poprzednia</button>
+   <button class="btn btn-success" onclick="location.href=href='page1.php'">nastepna</button>
    <?php
         echo '<br>'.$_SESSION['imie'].'<br>'.$_SESSION['nazwisko'].'<br>'.$_SESSION['plec'].'<br>'.$_SESSION['wiek'].'<br>';
    ?>
